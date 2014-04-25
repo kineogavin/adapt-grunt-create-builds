1293b4ad78e0291e019a06c13beab12d4e0831cd
kineogavin <gavin.mcmaster@kineo.com>
2014-04-25 15:59:04 +0100

    initial commit

---
+++ tasks/adapt_grunt_create_builds.js
@@ -0,0 +1,21 @@
+/*
+ * adapt-grunt-create-builds
+ * https://github.com/kineogavin/adapt-grunt-create-builds
+ *
+ * Copyright (c) 2014 kineogavin
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('adapt_grunt_create_builds', 'Copies from src and courses folder to create one or more Adapt builds', function() {
+    
+    
+  });
+
+};
